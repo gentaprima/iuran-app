3e5a4af2e6a319108aaec61f3c7ef5d39bc98b8e
prasetya <prasetya2423@gmail.com>
2022-05-18 23:07:28 +0700

    add seeder for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\ModelUsers;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        ModelUsers::create(
+            [
+                'first_name'=>"phs",
+                'last_name'=>"hhahaha",
+                'phone_number'=>"0894382948932",
+                'email' =>'prasetyahs@gmail.com',
+                'password'=>Hash::make('asdasd'),
+                'role'=>1
+            ]
+        );      
     }
 }
